bc9b338979e7a4f7853a956514017a2cce12a3b3
Filipe Barros <filipe.araujo9@outlook.com>
2022-04-14 20:20:39 -0300

    first layout

---
+++ assets/JS/main.js
@@ -0,0 +1,42 @@
+
+changePoster = () => {
+  const moviesPoster = [
+    {
+      name: 'THE BATMAN',
+      url: './assets/IMG/moviesPoster/batman.png',
+      age:'./assets/IMG/age/fourten.png'
+    },
+    {
+      name: 'AWAKE',
+      url: './assets/IMG/moviesPoster/awake.jpg',
+      age:'./assets/IMG/age/sixten.png'
+    },
+    {
+      name: 'NÃO OLHE PRA CIMA',
+      url: './assets/IMG/moviesPoster/naoOlhe.jpg',
+      age:'./assets/IMG/age/sixten.png'
+    },
+    {
+      name: 'ARMY OF DEAD',
+      url: './assets/IMG/moviesPoster/armyOfDead.jpg',
+      age:'./assets/IMG/age/eighten.png'
+    }
+  ];
+ 
+  i = getRandomIntInclusive(0,3)
+  
+
+  document.querySelector('#titlePoster').textContent  = moviesPoster[i].name;
+  document.querySelector('#posterIMG').src = moviesPoster[i].url;
+  document.querySelector('#age').src = moviesPoster[i].age;
+
+
+}
+
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+changePoster()
