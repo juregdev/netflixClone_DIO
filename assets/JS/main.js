4f67c66d7ef90a7e4f30cd475e288dcaed01680f
Filipe Barros <filipe.araujo9@outlook.com>
2022-04-17 20:21:54 -0300

    add carroussel

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -46,3 +46,71 @@ window.addEventListener('scroll', () => {
     document.querySelector('#headerMenu').style.background = ''
   }
 })
+
+
+const $next = document.querySelector(".arrowRight")
+const $prev = document.querySelector(".arrowLeft")
+let carrosselRated =0
+$next.addEventListener('click', () => {
+  $prev.style.display = 'flex'
+
+  if (carrosselRated >= 52.5){
+    carrosselRated=0;
+    $prev.style.display = 'none'
+    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+  } else {
+    carrosselRated+= 17.5
+    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+
+  }
+})
+
+$prev.addEventListener('click', () => {
+
+  if (carrosselRated <= 17.5){
+    carrosselRated=0;
+    $prev.nextElementSibling.style.transform = `translateX(${carrosselRated}%)`
+    $prev.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselRated-= 17.5
+  $prev.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+  
+  }
+})
+
+
+const $carrosselDrama = document.querySelector('.drama');
+const $nextDrama = document.querySelector("#arrowRightDrama")
+const $prevDrama = document.querySelector("#arrowLeftDrama")
+let carrosselDrama = 0
+
+$nextDrama.addEventListener('click', () => {
+  $prevDrama.style.display = 'flex'
+
+  if (carrosselDrama >= 52.5){
+    carrosselDrama=0;
+    $prevDrama.style.display = 'none'
+    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  } else {
+    carrosselDrama+= 17.5
+    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  }
+
+})
+
+$prevDrama.addEventListener('click', () => {
+
+  if (carrosselDrama <= 17.5){
+    carrosselDrama=0;
+    $prevDrama.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
+    $prevDrama.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselDrama-= 17.5
+    $prevDrama.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  
+  }
+})
