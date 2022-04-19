0033c5a09ac954bb63e2f0a427e46b42f112298b
Filipe Barros <filipe.araujo9@outlook.com>
2022-04-19 20:21:47 -0300

    new logical file

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,310 +3,123 @@ changePoster = () => {
     {
       name: 'THE BATMAN',
       url: './assets/IMG/moviesPoster/batman.png',
-      age:'./assets/IMG/age/fourten.png'
+      age: './assets/IMG/age/fourten.png'
     },
     {
       name: 'AWAKE',
       url: './assets/IMG/moviesPoster/awake.jpg',
-      age:'./assets/IMG/age/sixten.png'
+      age: './assets/IMG/age/sixten.png'
     },
     {
       name: 'NÃO OLHE PRA CIMA',
       url: './assets/IMG/moviesPoster/naoOlhe.jpg',
-      age:'./assets/IMG/age/sixten.png'
+      age: './assets/IMG/age/sixten.png'
     },
     {
       name: 'ARMY OF DEAD',
       url: './assets/IMG/moviesPoster/armyOfDead.jpg',
-      age:'./assets/IMG/age/eighten.png'
+      age: './assets/IMG/age/eighten.png'
     }
-  ];
- 
-  i = getRandomIntInclusive(0,3)
-  document.querySelector('#titlePoster').textContent  = moviesPoster[i].name;
-  document.querySelector('#posterIMG').src = moviesPoster[i].url;
-  document.querySelector('#age').src = moviesPoster[i].age;
+  ]
+
+  i = getRandomIntInclusive(0, 3)
+  document.querySelector('#titlePoster').textContent = moviesPoster[i].name
+  document.querySelector('#posterIMG').src = moviesPoster[i].url
+  document.querySelector('#age').src = moviesPoster[i].age
 }
 
-function getRandomIntInclusive (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 changePoster()
 
-document.querySelector('#headerMenu').style.background = 'transparent';
+let $arrowRight = document.querySelectorAll('.arrowRight')
+i = [0, 0, 0, 0]
 
-
-window.addEventListener('scroll', () => {
-  let topWindow = window.scrollY;
-  
-  if (topWindow == 0 ){
-    document.querySelector('#headerMenu').style.background = 'transparent';
-  } else {
-    document.querySelector('#headerMenu').style.background = ''
-  }
-})
-
-
-const $next = document.querySelector(".arrowRight")
-const $prev = document.querySelector(".arrowLeft")
-let carrosselRated =0
-$next.addEventListener('click', () => {
-  $prev.style.display = 'flex'
-
-  if (carrosselRated >= 52.5){
-    carrosselRated=0;
-    $prev.style.display = 'none'
-    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
-  } else {
-    carrosselRated+= 17.5
-    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
-
-  }
-})
-
-$prev.addEventListener('click', () => {
-
-  if (carrosselRated <= 17.5){
-    carrosselRated=0;
-    $prev.nextElementSibling.style.transform = `translateX(${carrosselRated}%)`
-    $prev.style.display = 'none'
-    console.log(true)
-  }
-  else{
-    carrosselRated-= 17.5
-  $prev.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
-  
-  }
-})
-
-
-const $nextDrama = document.querySelector("#arrowRightDrama")
-const $prevDrama = document.querySelector("#arrowLeftDrama")
-let carrosselDrama = 0
-
-$nextDrama.addEventListener('click', () => {
-  $prevDrama.style.display = 'flex'
-
-  if (carrosselDrama >= 52.5){
-    carrosselDrama=0;
-    $prevDrama.style.display = 'none'
-    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  } else {
-    carrosselDrama+= 17.5
-    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  }
-
-})
-
-$prevDrama.addEventListener('click', () => {
-
-  if (carrosselDrama <= 17.5){
-    carrosselDrama=0;
-    $prevDrama.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
-    $prevDrama.style.display = 'none'
-    console.log(true)
-  }
-  else{
-    carrosselDrama-= 17.5
-    $prevDrama.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  
-  }
-})
-
-
-const $nextAction = document.querySelector("#arrowRightAction")
-const $prevAction = document.querySelector("#arrowLeftAction")
-let carrosselAction = 0
-
-$nextAction.addEventListener('click', () => {
-  $prevAction.style.display = 'flex'
-
-  if (carrosselDrama >= 52.5){
-    carrosselDrama=0;
-    $prevAction.style.display = 'none'
-    $nextAction.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  } else {
-    carrosselDrama+= 17.5
-    $nextAction.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  }
-
-})
-
-$prevAction.addEventListener('click', () => {
-
-  if (carrosselDrama <= 17.5){
-    carrosselDrama=0;
-    $prevAction.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
-    $prevAction.style.display = 'none'
-    console.log(true)
-  }
-  else{
-    carrosselDrama-= 17.5
-    $prevAction.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  
-  }
-})
-
-const $nextHorror = document.querySelector("#arrowRightHorror")
-const $prevHorror = document.querySelector("#arrowLeftHorror")
-let carrosselHorror = 0
-
-$nextHorror.addEventListener('click', () => {
-  $prevHorror.style.display = 'flex'
-
-  if (carrosselDrama >= 52.5){
-    carrosselDrama=0;
-    $prevHorror.style.display = 'none'
-    $nextHorror.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  } else {
-    carrosselDrama+= 17.5
-    $nextHorror.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  }
-
-})
-
-$prevHorror.addEventListener('click', () => {
-
-  if (carrosselDrama <= 17.5){
-    carrosselDrama=0;
-    $prevHorror.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
-    $prevHorror.style.display = 'none'
-    console.log(true)
-  }
-  else{
-    carrosselDrama-= 17.5
-    $prevHorror.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
-  
-  }
-})
-
-const apiPull = async () => {
-  const API_KEY = '?api_key=abe9ef82fb72277046d8e71df76e2720';
-  const BASE_URL = 'https://api.themoviedb.org/3/movie/changes';
-
-
- await axios.get(`${BASE_URL}${API_KEY}`).then(res =>{searchMovie(res.data.results)})
- 
-}
-
-
-const searchMovie = async (data) => {
-  const BASE_URLMovie = 'https://api.themoviedb.org/3/movie/';
-  const API_KEY = '?api_key=abe9ef82fb72277046d8e71df76e2720';
-  
-  for (i in data) {
-
-    if (document.querySelector('#inRated').children.length >= 23 && document.querySelector('#drama').children.length >= 23 && document.querySelector('#action').children.length >= 23 && document.querySelector('#horror').children.length >= 23) {
-      console.log('acabou')
-      return
-   } else {
-
-    try{
-      let movie = await axios.get(`${BASE_URLMovie}${data[i].id}${API_KEY}`)
-      card(movie.data)
-      
-    } catch(e){
-      console.error("GET ERROR")
+$arrowRight.forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    let arrowPosition = 0
+    switch (arrow.id) {
+      case '':
+        arrowPosition = 0
+        break
+      case 'arrowRightDrama':
+        arrowPosition = 1
+        break
+      case 'arrowRightAction':
+        arrowPosition = 2
+        break
+      case 'arrowRightHorror':
+        arrowPosition = 3
+        break
     }
-  }}
-}
+    i[arrowPosition] += valueWidth()
 
+    let $lastEl = arrow.nextElementSibling.nextElementSibling.lastElementChild
 
-
-const card = async (data) => {
-
-
-  if(document.querySelector('#inRated').children.length >= 23) {
-    document.querySelectorAll('.moviesCarrossel')[0].style.display = 'block'
-  }
-
-  if(document.querySelector('#drama').children.length >= 23) {
-    document.querySelectorAll('.moviesCarrossel')[1].style.display = 'block'
-   
-  }
-  if(document.querySelector('#action').children.length >= 23) {
-    document.querySelectorAll('.moviesCarrossel')[2].style.display = 'block'
-  }
-
-  if(document.querySelector('#horror').children.length >= 22) {
-    document.querySelectorAll('.moviesCarrossel')[3].style.display = 'block'
-  }
-
-  if (document.querySelector('#inRated').children.length >= 23 && document.querySelector('#drama').children.length >= 23 && document.querySelector('#action').children.length >= 23 && document.querySelector('#horror').children.length >= 23) {
-     return
-  }
-
-  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-  let $class = ''
-
-  if(data.backdrop_path == null) { return}
- 
-  for (i in data.genres) {
-    switch (data.genres[i].name) {
-      case 'Action':
-        $class = 'action'
-        break;
-      case 'Drama': 
-        $class = 'drama'
-        break;
-      case 'Horror':
-        $class = 'horror'
-        break;
-      default :
-        $class = 'inRated'
-        break;
+    if ($lastEl.getBoundingClientRect().x < window.innerWidth) {
+      arrow.nextElementSibling.style.display = 'none'
+      i[arrowPosition] = 0
+    } else {
+      arrow.nextElementSibling.style.display = 'flex'
     }
-  }
 
-   if(document.querySelector('#inRated').children.length >= 23 && $class == 'inRated') {
-    return
-  } else if($class == 'drama' && document.querySelector('#drama').children.length >= 23) {
-    return
-  } else if($class == 'action' && document.querySelector('#action').children.length >= 23){
-    return
+    arrow.nextElementSibling.nextElementSibling.style.transform = `translateX(-${i[arrowPosition]}px)`
+  })
+})
+
+let $arrowLeft = document.querySelectorAll('.arrowLeft')
+
+$arrowLeft.forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    let arrowPosition = 0
+    switch (arrow.id) {
+      case '':
+        arrowPosition = 0
+        break
+      case 'arrowLeftDrama':
+        arrowPosition = 1
+        break
+      case 'arrowLeftAction':
+        arrowPosition = 2
+        break
+      case 'arrowLeftHorror':
+        arrowPosition = 3
+        break
+    }
+    i[arrowPosition] -= valueWidth()
+    if (i[arrowPosition] == 0) arrow.style.display = 'none'
+    arrow.nextElementSibling.style.transform = `translateX(-${i[arrowPosition]}px`
+  })
+})
+
+const valueWidth = () => {
+  if (window.innerWidth < 1100) {
+    return (window.innerWidth - 24) / 2
   } else {
-  $card = `
-  <div class="card">
-  <img src="${IMG_URL}${data.backdrop_path}" alt="card">
-  <h1 class="nameFilm">${data.title}</h1>
-  <div class="btnCards">
-    <button class="playCard"><img src="./assets/IMG/play.png" alt=""></button>
-    <button class="moreBtn"><img src="./assets/IMG/morebtn.png" alt=""></button>
-    <button class="likeBtn">
-      <div>
-        <img src="./assets/IMG/dislike.png" alt="">
-        <img src="./assets/IMG/like.png" alt="">
-        <img src="./assets/IMG/veryLike.png" alt="">
-      </div>
-    </button>
-  </div>
-</div>
-  `
-  document.querySelector(`#${$class}`).innerHTML += $card
+    return document.querySelector('.carrossel').scrollWidth / 5 - 60
   }
 }
 
-apiPull()
-
-document.querySelectorAll('.moviesCarrossel')
-
-const $menu = document.querySelector(".dropMenu")
+const $menu = document.querySelector('.dropMenu')
 
 $menu.addEventListener('click', () => {
+  if (document.querySelector('#menu').style.transform == 'translateY(0px)') {
+    if (window.scrollY == 0) {
+      document.querySelector('#headerMenu').style = 'background:transparent;'
+    } else {
+      document.querySelector('#headerMenu').style = ''
+    }
 
-  if(document.querySelector('#menu').style.transform == 'translateY(0px)' ) {
-
-    if (window.scrollY == 0) {document.querySelector('#headerMenu').style = 'background:transparent;'} else {document.querySelector('#headerMenu').style = ''} 
-    
     document.querySelector('#menu').style.transform = ''
     document.querySelector('.accountInfo').style.transform = ''
-
   } else {
-
-  document.querySelector('#headerMenu').style = 'height:100vh; background:#141414'
-  document.querySelector('#menu').style.transform = 'translateY(0px)'
-  document.querySelector('.accountInfo').style.transform = 'translateY(0px)'}
+    document.querySelector('#headerMenu').style =
+      'height:100vh; background:#141414'
+    document.querySelector('#menu').style.transform = 'translateY(0px)'
+    document.querySelector('.accountInfo').style.transform = 'translateY(0px)'
+  }
 })
---
+++ assets/JS/mainLast.js
@@ -0,0 +1,319 @@
+changePoster = () => {
+  const moviesPoster = [
+    {
+      name: 'THE BATMAN',
+      url: './assets/IMG/moviesPoster/batman.png',
+      age:'./assets/IMG/age/fourten.png'
+    },
+    {
+      name: 'AWAKE',
+      url: './assets/IMG/moviesPoster/awake.jpg',
+      age:'./assets/IMG/age/sixten.png'
+    },
+    {
+      name: 'NÃO OLHE PRA CIMA',
+      url: './assets/IMG/moviesPoster/naoOlhe.jpg',
+      age:'./assets/IMG/age/sixten.png'
+    },
+    {
+      name: 'ARMY OF DEAD',
+      url: './assets/IMG/moviesPoster/armyOfDead.jpg',
+      age:'./assets/IMG/age/eighten.png'
+    }
+  ];
+ 
+  i = getRandomIntInclusive(0,3)
+  document.querySelector('#titlePoster').textContent  = moviesPoster[i].name;
+  document.querySelector('#posterIMG').src = moviesPoster[i].url;
+  document.querySelector('#age').src = moviesPoster[i].age;
+}
+
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+changePoster()
+
+document.querySelector('#headerMenu').style.background = 'transparent';
+
+
+window.addEventListener('scroll', () => {
+  let topWindow = window.scrollY;
+  
+  if (topWindow == 0 ){
+    document.querySelector('#headerMenu').style.background = 'transparent';
+  } else {
+    document.querySelector('#headerMenu').style.background = ''
+  }
+})
+
+
+const $next = document.querySelector(".arrowRight")
+const $prev = document.querySelector(".arrowLeft")
+let carrosselRated =0
+$next.addEventListener('click', () => {
+  $prev.style.display = 'flex'
+
+  if (carrosselRated >= 52.5){
+    carrosselRated=0;
+    $prev.style.display = 'none'
+    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+  } else {
+    carrosselRated+= 17.5
+    $next.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+
+  }
+})
+
+$prev.addEventListener('click', () => {
+
+  if (carrosselRated <= 17.5){
+    carrosselRated=0;
+    $prev.nextElementSibling.style.transform = `translateX(${carrosselRated}%)`
+    $prev.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselRated-= 17.5
+  $prev.nextElementSibling.style.transform = `translateX(-${carrosselRated}%)`;
+  
+  }
+})
+
+
+const $nextDrama = document.querySelector("#arrowRightDrama")
+const $prevDrama = document.querySelector("#arrowLeftDrama")
+let carrosselDrama = 0
+
+$nextDrama.addEventListener('click', () => {
+  $prevDrama.style.display = 'flex'
+
+  if (carrosselDrama >= 52.5){
+    carrosselDrama=0;
+    $prevDrama.style.display = 'none'
+    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  } else {
+    carrosselDrama+= 17.5
+    $nextDrama.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  }
+
+})
+
+$prevDrama.addEventListener('click', () => {
+
+  if (carrosselDrama <= 17.5){
+    carrosselDrama=0;
+    $prevDrama.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
+    $prevDrama.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselDrama-= 17.5
+    $prevDrama.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  
+  }
+})
+
+
+const $nextAction = document.querySelector("#arrowRightAction")
+const $prevAction = document.querySelector("#arrowLeftAction")
+let carrosselAction = 0
+
+$nextAction.addEventListener('click', () => {
+  $prevAction.style.display = 'flex'
+
+  if (carrosselDrama >= 52.5){
+    carrosselDrama=0;
+    $prevAction.style.display = 'none'
+    $nextAction.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  } else {
+    carrosselDrama+= 17.5
+    $nextAction.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  }
+
+})
+
+$prevAction.addEventListener('click', () => {
+
+  if (carrosselDrama <= 17.5){
+    carrosselDrama=0;
+    $prevAction.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
+    $prevAction.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselDrama-= 17.5
+    $prevAction.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  
+  }
+})
+
+const $nextHorror = document.querySelector("#arrowRightHorror")
+const $prevHorror = document.querySelector("#arrowLeftHorror")
+let carrosselHorror = 0
+
+$nextHorror.addEventListener('click', () => {
+  $prevHorror.style.display = 'flex'
+
+  if (carrosselDrama >= 52.5){
+    carrosselDrama=0;
+    $prevHorror.style.display = 'none'
+    $nextHorror.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  } else {
+    carrosselDrama+= 17.5
+    $nextHorror.nextElementSibling.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  }
+
+})
+
+$prevHorror.addEventListener('click', () => {
+
+  if (carrosselDrama <= 17.5){
+    carrosselDrama=0;
+    $prevHorror.nextElementSibling.style.transform = `translateX(${carrosselDrama}%)`
+    $prevHorror.style.display = 'none'
+    console.log(true)
+  }
+  else{
+    carrosselDrama-= 17.5
+    $prevHorror.nextElementSibling.style.transform = `translateX(-${carrosselDrama}%)`;
+  
+  }
+})
+
+const apiPull = async () => {
+  const API_KEY = '?api_key=abe9ef82fb72277046d8e71df76e2720';
+  const BASE_URL = 'https://api.themoviedb.org/3/movie/changes';
+
+
+ await axios.get(`${BASE_URL}${API_KEY}`).then(res =>{searchMovie(res.data.results)
+console.log(`${BASE_URL}${API_KEY}`)
+})
+ 
+}
+
+
+const searchMovie = async (data) => {
+  const BASE_URLMovie = 'https://api.themoviedb.org/3/movie/';
+  const API_KEY = '?api_key=abe9ef82fb72277046d8e71df76e2720';
+
+
+  
+  for (i in data) {
+
+    if (document.querySelector('#inRated').children.length >= 23 && document.querySelector('#drama').children.length >= 23 && document.querySelector('#action').children.length >= 23 && document.querySelector('#horror').children.length >= 23) {
+      console.log('acabou')
+      return
+   } else {
+
+    try{
+      let movie = await axios.get(`${BASE_URLMovie}${data[i].id}${API_KEY}`)
+      //card(movie.data)
+    
+      
+    } catch(e){
+      console.log(`${BASE_URLMovie}${data[i].id}${API_KEY}`)
+      console.log(e)
+    }
+  }}
+}
+
+
+
+const card = async (data) => {
+
+
+  if(document.querySelector('#inRated').children.length >= 23) {
+    document.querySelectorAll('.moviesCarrossel')[0].style.display = 'block'
+  }
+
+  if(document.querySelector('#drama').children.length >= 23) {
+    document.querySelectorAll('.moviesCarrossel')[1].style.display = 'block'
+   
+  }
+  if(document.querySelector('#action').children.length >= 23) {
+    document.querySelectorAll('.moviesCarrossel')[2].style.display = 'block'
+  }
+
+  if(document.querySelector('#horror').children.length >= 22) {
+    document.querySelectorAll('.moviesCarrossel')[3].style.display = 'block'
+  }
+
+  if (document.querySelector('#inRated').children.length >= 23 && document.querySelector('#drama').children.length >= 23 && document.querySelector('#action').children.length >= 23 && document.querySelector('#horror').children.length >= 23) {
+     return
+  }
+
+  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+  let $class = ''
+
+  if(data.backdrop_path == null) { return}
+ 
+  for (i in data.genres) {
+    switch (data.genres[i].name) {
+      case 'Action':
+        $class = 'action'
+        break;
+      case 'Drama': 
+        $class = 'drama'
+        break;
+      case 'Horror':
+        $class = 'horror'
+        break;
+      default :
+        $class = 'inRated'
+        break;
+    }
+  }
+
+   if(document.querySelector('#inRated').children.length >= 23 && $class == 'inRated') {
+    return
+  } else if($class == 'drama' && document.querySelector('#drama').children.length >= 23) {
+    return
+  } else if($class == 'action' && document.querySelector('#action').children.length >= 23){
+    return
+  } else {
+  $card = `
+  <div class="card">
+  <img src="${IMG_URL}${data.backdrop_path}" alt="card">
+  <h1 class="nameFilm">${data.title}</h1>
+  <div class="btnCards">
+    <button class="playCard"><img src="./assets/IMG/play.png" alt=""></button>
+    <button class="moreBtn"><img src="./assets/IMG/morebtn.png" alt=""></button>
+    <button class="likeBtn">
+      <div>
+        <img src="./assets/IMG/dislike.png" alt="">
+        <img src="./assets/IMG/like.png" alt="">
+        <img src="./assets/IMG/veryLike.png" alt="">
+      </div>
+    </button>
+  </div>
+</div>
+  `
+  document.querySelector(`#${$class}`).innerHTML += $card
+  }
+}
+
+apiPull()
+
+document.querySelectorAll('.moviesCarrossel')
+
+const $menu = document.querySelector(".dropMenu")
+
+$menu.addEventListener('click', () => {
+
+  if(document.querySelector('#menu').style.transform == 'translateY(0px)' ) {
+
+    if (window.scrollY == 0) {document.querySelector('#headerMenu').style = 'background:transparent;'} else {document.querySelector('#headerMenu').style = ''} 
+    
+    document.querySelector('#menu').style.transform = ''
+    document.querySelector('.accountInfo').style.transform = ''
+
+  } else {
+
+  document.querySelector('#headerMenu').style = 'height:100vh; background:#141414'
+  document.querySelector('#menu').style.transform = 'translateY(0px)'
+  document.querySelector('.accountInfo').style.transform = 'translateY(0px)'}
+})
+
